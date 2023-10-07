3d7c5670d1a43f5bda14e91bedfbec4af91f9b36
nerock <manueladalidmoya@gmail.com>
2023-10-07 18:52:57 +0200

    add account storage test

--- assignment_01/account/go.mod
+++ assignment_01/account/go.mod
@@ -5,6 +5,7 @@ go 1.21.1
 require github.com/smartystreets/goconvey v1.8.1
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/smarty/assertions v1.15.0 // indirect
---
+++ assignment_01/account/internal/storage/accountpostgres_test.go
@@ -0,0 +1,52 @@
+package storage
+
+import (
+	"context"
+	"errors"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/nerock/omi-test/account/internal"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestAccountPostgresStorage_UpdateAccountByID(t *testing.T) {
+	Convey("UpdateAccountByID", t, func() {
+		ctx := context.Background()
+		db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer db.Close()
+		st := NewAccountPostgresStorage(db)
+
+		query := "UPDATE accounts SET name=$2 WHERE id = $1"
+		acc := internal.Account{ID: "id", Name: "name"}
+		Convey("when the database returns an error", func() {
+			mock.ExpectExec(query).
+				WillReturnError(errors.New("something happened"))
+
+			Convey("the storage should return an error", func() {
+				So(st.UpdateAccountByID(ctx, acc), ShouldNotBeNil)
+			})
+		})
+
+		Convey("when the database returns an invalid result", func() {
+			mock.ExpectExec(query).
+				WillReturnResult(sqlmock.NewResult(0, 0))
+
+			Convey("the storage should return an error", func() {
+				So(st.UpdateAccountByID(ctx, acc), ShouldNotBeNil)
+			})
+		})
+
+		Convey("when the database returns a valid result", func() {
+			mock.ExpectExec(query).
+				WillReturnResult(sqlmock.NewResult(0, 1))
+
+			Convey("the storage should return no error", func() {
+				So(st.UpdateAccountByID(ctx, acc), ShouldBeNil)
+			})
+		})
+	})
+}
