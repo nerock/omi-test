65abc74e77392cfd4a8189f100795c2d250254aa
nerock <manueladalidmoya@gmail.com>
2023-10-07 19:18:59 +0200

    add account api implementation

--- assignment_01/account/internal/api/accountgrpc.go
+++ assignment_01/account/internal/api/accountgrpc.go
@@ -2,8 +2,11 @@ package api
 
 import (
 	"context"
+	"errors"
 	"github.com/nerock/omi-test/account/internal"
 	pb "github.com/nerock/omi-test/account/pb/account"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type AccountGrpcApi struct {
@@ -17,5 +20,15 @@ func NewAccountGrpcApi(svc internal.AccountService) *AccountGrpcApi {
 }
 
 func (a *AccountGrpcApi) UpdateAccount(ctx context.Context, req *pb.UpdateAccountRequest) (*pb.UpdateAccountResponse, error) {
-	return nil, nil
+	if req.GetId() == "" || req.GetName() == "" {
+		return nil, status.Error(codes.InvalidArgument, "invalid account parameters")
+	}
+
+	if _, err := a.svc.UpdateAccount(ctx, req.GetId(), req.GetName()); errors.Is(err, internal.ErrInvalidAccount) {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	} else if err != nil {
+		return nil, status.Error(codes.Internal, "could not update account")
+	}
+
+	return &pb.UpdateAccountResponse{Success: true}, nil
 }
