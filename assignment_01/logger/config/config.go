cdd58bfbde8059ba1beee37401e601557e8037d0
nerock <manueladalidmoya@gmail.com>
2023-10-08 12:48:32 +0200

    add config to audit log service

---
+++ assignment_01/logger/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	Postgres PostgresConfig `json:"postgres"`
+	Log      LogConfig      `json:"log"`
+	Nats     NatsConfig     `json:"nats"`
+}
+
+type PostgresConfig struct {
+	User     string `json:"user"`
+	Password string `json:"password"`
+	DB       string `json:"db"`
+}
+
+type LogConfig struct {
+	Level      int  `json:"level"`
+	StackTrace bool `json:"stacktrace"`
+}
+
+type NatsConfig struct {
+	Uri   string `json:"uri"`
+	Topic string `json:"topic"`
+}
+
+func FromFile(path string) (Config, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return Config{}, fmt.Errorf("could not read config file: %w", err)
+	}
+	defer f.Close()
+
+	var cfg Config
+	if err := json.NewDecoder(f).Decode(&cfg); err != nil {
+		return Config{}, fmt.Errorf("could not decode config json: %w", err)
+	}
+
+	return cfg, nil
+}
